bf6c534b4c4a41ce0248e12e8c3fa5f4e89d20a2
dimitrisx26 <dxynos@outlook.com>
2023-12-29 21:35:08 +0200

    Add meta tags for SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Dimitrios Xynos';
-  description =
-    'Dimitrios Xynos is a Computer Science undergraduate specializing in Front-End Development. With expertise in HTML, CSS, JavaScript, and Angular, Dimitrios creates visually appealing and user-friendly websites. Experience in data entry, front-end development, and social media management. Visit the site to explore projects and connect with Dimitrios.';
+  constructor(private metaService: Meta) {
+    this.metaService.addTag({
+      name: 'title',
+      content: 'Dimitrios Xynos - Front-End Developer',
+    });
+    this.metaService.addTag({
+      name: 'description',
+      content:
+        'Dimitrios Xynos is a Computer Science undergraduate specializing in Front-End Development. With expertise in HTML, CSS, JavaScript, and Angular, Dimitrios creates visually appealing and user-friendly websites. Experience in data entry, front-end development, and social media management. Visit the site to explore projects and connect with Dimitrios.',
+    });
+    this.metaService.addTag({
+      name: 'keywords',
+      content:
+        'Dimitrios Xynos, Dimitrios, Xynos, Front-End Developer, Front-End, Developer, HTML, CSS, JavaScript, Angular, Computer Science, Computer, Science, Undergraduate, Undergrad, Undergraduate Student, Student, Web Developer, Web, Web Development, Web Dev, Web Developer, Web Design, Web Designer, Web Design and Development, Web Design and Dev, Web Design and Developer, Web Design and Designer, Web Designer and Developer, Web Designer and Dev, Web Designer and Design, Web Dev and Design, Web Dev and Developer, Web Dev and Designer, Web Dev and Development, Web Development and Design, Web Development and Dev, Web Development and Developer, Web Development and Designer, Web Development and Development, Web Dev and Dev, Web Dev and Development, Web Dev and Designer, Web Dev and Design, Web Dev and Developer, Web Design and Dev, Web Design and Development, Web Design and Designer, Web Design and Design, Web Design and Developer',
+    });
+    this.metaService.addTag({
+      name: 'author',
+      content: 'Dimitrios Xynos',
+    });
+  }
 }
