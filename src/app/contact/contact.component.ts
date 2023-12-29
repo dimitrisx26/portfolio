43d2d3b2ddbf05824d624547c4821f607d8aa6af
dimitrisx26 <dxynos@outlook.com>
2023-12-29 11:05:43 +0200

    Update contact form with form validation and submission handling

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
+  contactForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', Validators.required),
+  });
+
+  submitErrorMessage = '';
+  submitSuccessMessage = '';
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      // Send form data to server or handle it in some other way
+      // ...
+
+      this.submitSuccessMessage = 'Form submission successful!';
+      this.submitErrorMessage = '';
+    } else {
+      this.submitErrorMessage = 'Error sending message!';
+      this.submitSuccessMessage = '';
+    }
+  }
 
 }
