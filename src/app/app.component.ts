c09f242e6d1c6f844fb5a0efe9e497dc56004969
dimitrisx26 <dxynos@outlook.com>
2023-12-29 21:22:09 +0200

    Add description to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dimitrios Xynos';
+  description =
+    'Dimitrios Xynos is a Computer Science undergraduate specializing in Front-End Development. With expertise in HTML, CSS, JavaScript, and Angular, Dimitrios creates visually appealing and user-friendly websites. Experience in data entry, front-end development, and social media management. Visit the site to explore projects and connect with Dimitrios.';
 }
